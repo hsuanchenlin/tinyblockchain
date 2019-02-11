4fd974218d7ea4271cfbb906893c0720231797e6
ken <hsuanchen@outlook.com>
2019-02-12 00:33:58 +0800

    clean mining

--- src/mine/miner.go
+++ src/mine/miner.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"github.com/ethereum/go-ethereum/crypto"
+	"time"
 )
 
 type Miner struct {
@@ -16,22 +17,32 @@ type Miner struct {
 	threadNumber       uint8
 	nonce              uint32
 }
-func (miner *Miner) mine() (bool, int) {
-	nonce := 0
+
+func (miner *Miner) mine(c chan uint32, thNum int, nonce uint32) (bool, uint32) {
+	fmt.Printf("thread %d\n", thNum)
 	for {
-		nonce++
+		if miner.nonce != 0 {
+			break
+		}
+		nonce += uint32(thNum)
 		seed := make([]byte, 40)
 		seed = crypto.Keccak512(seed)
 		seed = append(seed, byte(nonce))
 		blkHash :=  miner.block.Header.BlockHash[:]
 		hashRes := crypto.Keccak256(append(seed,  blkHash...))
-		s := fmt.Sprintf("%s", hex.EncodeToString(hashRes))
-		fmt.Println(s)
-		fmt.Println("------")
+
 		if miningHashCmp(miner.targetPrefixZeroes, hashRes) {
+			miner.nonce = nonce
+			s := fmt.Sprintf("%s", hex.EncodeToString(hashRes))
+			fmt.Println("success routine")
+			fmt.Println(s)
+			fmt.Printf("%d %d\n",s[7], s[8])
+			fmt.Println("------")
+			c <- nonce
 			return true, nonce
 		}
 	}
+	fmt.Printf("fail routine %d\n", thNum)
 	return false, 0
 }
 
@@ -40,25 +51,50 @@ func (self *Miner) update() {
 }
 
 func (miner *Miner) start() {
+	var i uint8
+	c := make(chan uint32)
 
-}
-
-func decodeHex(s string) []byte {
-	b, err := hex.DecodeString(s)
-	if err != nil {
-		panic(err)
+	for i = 0; i < miner.threadNumber; i++{
+		go miner.mine(c, int(i), uint32(int(i)*1000000000))
+	}
+	timeEla := 0
+	for {
+		select {
+			case n := <-c:
+				fmt.Printf("done nonce:%d\n",n)
+				return
+			case <-time.After(time.Second * 1):
+				timeEla++
+				fmt.Printf("timeout %d\n", timeEla)
+		}
 	}
 
-	return b
 }
 
 func miningHashCmp(targetPrefixZeroes uint8, miningHash []byte) bool{
-	var i uint8
+	var i, prev uint8
 	shash := hex.EncodeToString(miningHash)
+	prev = shash[0]
 	for i=0; i < targetPrefixZeroes; i++ {
-		if shash[i] != '0' {
+		if  prev != shash[i] {
 			return false
 		}
+		prev = shash[i]
 	}
 	return true
+}
+
+
+func hex2int(ch rune) int {
+	chInt := int(ch)
+	if chInt >= 48 && chInt <= 57 {
+		return chInt - 48
+	}
+	if chInt >= 65 && chInt <= 70 {
+		return chInt - 55
+	}
+	if chInt >= 97 && chInt <= 102 {
+		return chInt - 87
+	}
+	return -1
 }
--- src/mine/miner_test.go
+++ src/mine/miner_test.go
@@ -17,6 +17,17 @@ func TestMineSimple(*testing.T) {
 	header := types.Header{nil, bh, bh, 0}
 	blk := types.Block{  &header}
 	miner := Miner{0, 1, &blk, bh, 0, 0}
-	res, nonce := miner.mine()
+	ch := make(chan uint32)
+	res, nonce := miner.mine(ch, 1,0)
 	fmt.Println(res,nonce)
+}
+
+func TestMineMulti(*testing.T) {
+	s := "abcdefghijabcdefghijabcdefghijab"
+	var bh [32]byte
+	copy(bh[:], s)
+	header := types.Header{nil, bh, bh, 0}
+	blk := types.Block{  &header}
+	miner := Miner{0, 4, &blk, bh, 4, 0}
+	miner.start()
 }
